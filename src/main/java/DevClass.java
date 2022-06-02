e3966cd1da91408a870d9246661f88c751d42224
manmarziya1995 <106771323+manmarziya1995@users.noreply.github.com>
2022-06-03 00:21:01 +0530

    Update DevClass.java
    Changes done

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass+new");
+        System.out.println("DevClass-new");
         //Checkin from mamarziya
     }
 }
